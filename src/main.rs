1bbf79159cd5c550d0a8dbec126599b796d69cfa
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-21 23:32:27 +0100

    general user input updates

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::io::{self};
+use std::any::{type_name};
+use std::io::{self,};
 use std::net::TcpStream;
 use std::thread;
 use serde_yaml::{from_str, Value};
@@ -7,6 +8,7 @@ use stblib::colors::*;
 use stblib::strings::Strings;
 use crate::config::{Config, config_open};
 
+
 mod recv;
 mod send;
 
@@ -14,7 +16,13 @@ mod config;
 mod formatter;
 mod constants;
 
-fn user_server_list(string_loader: &Strings, _config: &Config) {
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+}
+
+fn user_server_list(string_loader: &Strings, _config: &Config) -> i8 {
+    let mut enable_auto_login = false;
+
     println!("{BOLD}--- {CYAN}Strawberry Chat ({}){C_RESET} ---", constants::VERSION);
     println!("{GREEN}{}{C_RESET}\n", string_loader.str("Welcome"));
     println!("{BOLD}{CYAN}{UNDERLINE}{}{C_RESET}", string_loader.str("YourChatServers"));
@@ -31,12 +39,45 @@ fn user_server_list(string_loader: &Strings, _config: &Config) {
         );
     }
 
-    println!("{BOLD}{BLUE}[{}]{C_RESET}{BOLD} {}{C_RESET}", server_data_length + 1, string_loader.str("Custom"));
+    println!("{BOLD}{BLUE}[{}]{C_RESET}{BOLD} {}{C_RESET}\n", server_data_length + 1, string_loader.str("Custom"));
 
     let mut line_reader = rustyline::DefaultEditor::new().unwrap();
-    let promt = format!("{}", string_loader.str("SelChatServer"));
-    let server_selection = line_reader.readline(promt.as_str()).expect(string_loader.str("Aborted").as_str());
 
+    let prompt = format!("{}", string_loader.str("SelChatServer"));
+    let aborted = format!("{BOLD}{YELLOW}{}{C_RESET}", string_loader.str("Aborted"));
+
+    let server_selection: u8 = match line_reader.readline(&prompt) {
+        Ok(line) => match line.trim().parse() {
+            Ok(value) => value,
+            Err(_) => {
+                eprintln!(
+                    "{BOLD}{RED}{}{C_RESET}",
+                    format!("{}", string_loader.str("InvalidInput"))
+                );
+                std::process::exit(1);
+            }
+        },
+        Err(_) => {
+            eprintln!("{}", aborted.as_str());
+            std::process::exit(1);
+        }
+    };
+
+    if server_selection == (server_data_length + 1) as u8 {
+        let host = line_reader.readline(string_loader.str("Ipaddr").as_str()).expect(aborted.as_str());
+        let port: String = line_reader.readline(string_loader.str("Port").as_str()).expect(aborted.as_str());
+
+        -1
+    }
+
+    else if server_selection > (server_data_length + 1) as u8 {
+        eprintln!("{BOLD}{RED}{}{C_RESET}", string_loader.str("InvalidServerSelection"));
+        std::process::exit(1);
+    }
+
+    else {
+        (server_selection - 1) as i8
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -49,7 +90,11 @@ fn main() -> io::Result<()> {
         server_id = config.autoserver.server_id as usize;
     }
     else {
-        user_server_list(&string_loader, &config);
+        server_id = user_server_list(&string_loader, &config) as usize;
+    }
+
+    if server_id as i8 == -1 {
+        std::process::exit(0);
     }
 
 
@@ -60,11 +105,9 @@ fn main() -> io::Result<()> {
 
     let host = format!("{}:{}", server_config.address, server_config.port);
 
-    let stream = TcpStream::connect(host).expect("Error opening stream");
+    let stream = TcpStream::connect(host).expect(format!("{BOLD}{RED}{}{C_RESET}", string_loader.str("ErrNotReachable")).as_str());
 
     let send_stream = stream.try_clone().unwrap();
-
-
 
     let handler = thread::spawn(|| recv::recv(stream, config, server_config, string_loader));
     thread::spawn(|| send::send(send_stream, send_config, send_server_config));
