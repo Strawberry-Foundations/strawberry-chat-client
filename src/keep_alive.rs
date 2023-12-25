20a6c2d232a9ea8abc2ad86ca127e9041c2dbf31
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-25 13:39:26 +0100

    Fixed formatting

--- src/keep_alive.rs
+++ src/keep_alive.rs
@@ -4,9 +4,7 @@ use std::net::TcpStream;
 pub fn keep_alive(mut stream: TcpStream) {
     loop {
         stblib::utilities::sleep(30);
-        stream
-            .write_all(b"[#<keepalive.event.sent>]")
-            .expect("Failed to send Keep Alive");
+        stream.write_all(b"[#<keepalive.event.sent>]").expect("Failed to send Keep Alive");
         // println!("Heartbeat sent");
     }
 }
