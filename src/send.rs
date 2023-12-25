04cccf7fd981f5f304a2788e06651701f93e4ee6
Julian <81520713+Juliandev02@users.noreply.github.com>
2023-12-25 13:27:41 +0100

    Fixed formatting

--- src/send.rs
+++ src/send.rs
@@ -13,33 +13,24 @@ pub(crate) fn send(mut stream: TcpStream, config: Config, server_config: ServerV
     let mut line_reader = rustyline::DefaultEditor::new().unwrap();
 
     if server_config.autologin == true {
-        stream
-            .write(server_config.credentials.username.as_bytes())
-            .expect("Error writing stream");
+        stream.write(server_config.credentials.username.as_bytes()).expect("Error writing stream");
 
         stblib::utilities::ms_sleep(500);
 
-        stream
-            .write(server_config.credentials.password.as_bytes())
-            .expect("Error writing stream");
+        stream.write(server_config.credentials.password.as_bytes()).expect("Error writing stream");
     }
 
     loop {
         let input: String = match line_reader.readline("") {
             Ok(inp) => inp,
             Err(_) => {
-                eprintln!(
-                    "{}",
-                    format!("{BOLD}{YELLOW}{}{C_RESET}", string_loader.str("Aborted"))
-                );
+                eprintln!("{}",format!("{BOLD}{YELLOW}{}{C_RESET}", string_loader.str("Aborted")));
                 std::process::exit(1)
             }
         };
 
         line_reader.add_history_entry(&input).unwrap();
-        stream
-            .write(input.as_bytes())
-            .expect("Error writing stream");
+        stream.write(input.as_bytes()).expect("Error writing stream");
 
         delete_last_line();
     }
