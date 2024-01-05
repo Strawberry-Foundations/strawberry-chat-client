76e28856a13e4b2290deec36e547e7c487bd5c8c
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-05 23:07:32 +0100

    Complete refactoring

--- src/communication/keep_alive.rs
+++ src/communication/keep_alive.rs
@@ -5,6 +5,5 @@ pub fn keep_alive(mut stream: TcpStream) {
     loop {
         stblib::utilities::sleep(30);
         stream.write_all(b"[#<keepalive.event.sent>]").expect("Failed to send Keep Alive");
-        // println!("Heartbeat sent");
     }
 }
