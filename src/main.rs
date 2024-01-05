fa8d08b14092753c173ad0c3a663a916b52f13b9
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-01-05 23:01:23 +0100

    Complete refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,30 @@
 #![warn(clippy::all, clippy::nursery)]
 #![allow(clippy::missing_const_for_fn)]
 
-use std::env;
 use std::net::TcpStream;
-use std::path::{Path, PathBuf};
 use std::thread;
 
-use lazy_static::lazy_static;
 use owo_colors::OwoColorize;
-use stblib::strings::Strings;
 use std::sync::mpsc::channel;
+use crate::communication::keep_alive;
 
-use crate::config::{Config, get_lang_cfg, ServerValues};
+use crate::global::{CONFIG, SERVER_CONFIG, STRING_LOADER};
 
-mod recv;
-mod send;
+mod communication {
+    pub mod recv;
+    pub mod legacy_recv;
+    pub mod send;
+    pub mod keep_alive;
+}
 
 mod client_meta;
 mod config;
 mod constants;
 mod formatter;
-mod keep_alive;
 mod user_server_list;
 mod utilities;
 mod error_handler;
-
-lazy_static! {
-    pub static ref CONFIG: Config = {
-        let exe_path = env::current_exe().expect("Could not get your Strawberry Chat Client Executable");
-
-        let exe_dir = exe_path.parent().expect("Error determining the directory of the executable file.");
-
-        let exe_dir_str = PathBuf::from(exe_dir).display().to_string();
-
-        let mut config_path = format!("{exe_dir_str}/config.yml");
-
-        if !Path::new(&config_path).exists() {
-            config_path = String::from("./config.yml")
-        }
-
-        Config::new(config_path)
-    };
-
-    pub static ref STRING_LOADER: Strings = Strings::new(CONFIG.language.as_str(), &get_lang_cfg());
-
-    pub static ref SERVER_CONFIG: ServerValues = {
-        let server_id = match CONFIG.autoserver.enabled {
-            true => CONFIG.autoserver.server_id,
-            false => user_server_list::user_server_list(&CONFIG.path).unwrap_or_else(|_| {
-                eprintln!("{}", STRING_LOADER.str("Aborted").red().bold());
-                std::process::exit(1);
-            }),
-        };
-
-        if server_id == -1 {
-            std::process::exit(0);
-        }
-
-        Config::server_id(server_id, &CONFIG.path)
-    };
-
-}
+mod global;
 
 fn main() -> eyre::Result<()> {
     let (tx, rx) = channel::<()>();
@@ -81,12 +45,12 @@ fn main() -> eyre::Result<()> {
         thread::spawn(|| keep_alive::keep_alive(keep_alive_stream));
     }
 
-    let recv_handler = thread::spawn(move || recv::recv(&mut stream, tx).unwrap_or_else(|_| {
+    let recv_handler = thread::spawn(move || communication::recv::recv(&mut stream, tx).unwrap_or_else(|_| {
         eprintln!("{}", STRING_LOADER.str("ErrorRecvThread").red().bold());
         std::process::exit(1);
     }));
 
-    let send_handler = thread::spawn(|| send::send(send_stream, rx).unwrap_or_else(|_| {
+    let send_handler = thread::spawn(|| communication::send::send(send_stream, rx).unwrap_or_else(|_| {
         eprintln!("{}", STRING_LOADER.str("ErrorSendThread").red().bold());
         std::process::exit(1);
     }));
