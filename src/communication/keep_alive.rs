100f0a924a1922527cfc3bffdef366f49cd2794e
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-02 21:53:35 +0000

    Fixed non-pretty Keep alive error message

--- src/communication/keep_alive.rs
+++ src/communication/keep_alive.rs
@@ -1,9 +1,16 @@
 use std::io::Write;
 use std::net::TcpStream;
+use stblib::colors::{BOLD, C_RESET, RED};
 
 pub fn keep_alive(mut stream: TcpStream) {
     loop {
         stblib::utilities::sleep(30);
-        stream.write_all(b"[#<keepalive.event.sent>]").expect("Failed to send Keep Alive");
+        stream.write_all(b"[#<keepalive.event.sent>]").unwrap_or_else(|_| {
+            eprintln!(
+                "{BOLD}{RED}An error occurred when sending Keep Alive to the server.\n\
+                Could it be that the connection to the server has been lost?{C_RESET}"
+            );
+            return
+        });
     }
 }
