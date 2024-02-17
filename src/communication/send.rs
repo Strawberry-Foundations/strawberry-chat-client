0e1ed76826b4f9a6644ef65b10ad8f6295b6e6ab
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-17 17:35:49 +0100

    migration to new networking system

--- src/communication/send.rs
+++ src/communication/send.rs
@@ -1,19 +1,24 @@
-use std::io::Write;
-use std::net::TcpStream;
 use std::process::exit;
-use std::thread::sleep;
 use std::time::Duration;
 use std::sync::mpsc::Receiver;
 
-use eyre::{bail, Context};
+use tokio::net::TcpStream;
+use tokio::time::sleep;
+use tokio::io::WriteHalf;
+
 use rustyline::error::ReadlineError;
+
 use stblib::colors::*;
+use stblib::stbm::stbchat::net::OutgoingPacketStream;
+use stblib::stbm::stbchat::packet::{ClientsidePacket, ServersidePacket};
 
 use crate::{SERVER_CONFIG, STRING_LOADER};
+use crate::communication::login::login;
+use crate::object::login_packet::ServerLoginCredentialsPacketClient;
 use crate::utilities::delete_last_line;
 
 
-pub fn send(mut stream: TcpStream, rx: Receiver<()>) -> eyre::Result<()> {
+pub async fn send(mut w_server: OutgoingPacketStream<WriteHalf<TcpStream>>, rx: Receiver<String>) {
     if SERVER_CONFIG.autologin {
         println!("{GREEN}{BOLD}{}{C_RESET}\n", STRING_LOADER.str("AutologinActive"));
     } else {
@@ -21,36 +26,63 @@ pub fn send(mut stream: TcpStream, rx: Receiver<()>) -> eyre::Result<()> {
     }
 
     if !SERVER_CONFIG.compatibility_mode {
-        let _ = rx.recv().unwrap_or_else(|_| {
+        let tx_data = rx.recv().unwrap_or_else(|_| {
             println!("{BOLD}{YELLOW}{}{C_RESET}", STRING_LOADER.str("UnsuccessfulConnection"));
             exit(1);
         });
+
+        if tx_data == "event.login" {
+            let (username, password) = if SERVER_CONFIG.autologin {
+                (SERVER_CONFIG.credentials.username.clone(), SERVER_CONFIG.credentials.password.clone())
+            } else {
+                login()
+            };
+
+            w_server.write(ServersidePacket::Login {
+                username,
+                password
+            }).await.expect(&*STRING_LOADER.str("StreamWriteError"));
+        }
     }
 
     let mut line_reader = rustyline::DefaultEditor::new().unwrap();
 
     if SERVER_CONFIG.autologin && SERVER_CONFIG.compatibility_mode {
-        stream.write_all(SERVER_CONFIG.credentials.username.as_bytes()).context(STRING_LOADER.str("StreamWriteError"))?;
+        w_server.write(
+            SERVER_CONFIG.credentials.username.as_bytes())
+            .await
+            .expect(&*STRING_LOADER.str("StreamWriteError")
+            );
 
         stblib::utilities::ms_sleep(500);
 
-        stream.write_all(SERVER_CONFIG.credentials.password.as_bytes()).context(STRING_LOADER.str("StreamWriteError"))?;
+        w_server.write(
+            SERVER_CONFIG.credentials.password.as_bytes())
+            .await
+            .expect(&*STRING_LOADER.str("StreamWriteError")
+            );
     }
 
     loop {
         let input: String = match line_reader.readline("") {
             Ok(i) => i,
             Err(ReadlineError::Interrupted) => {
-                stream.write_all(b"/exit")?;
-                sleep(Duration::from_millis(300));
+                w_server.write(b"/exit").await.expect(&*STRING_LOADER.str("StreamWriteError"));
+                sleep(Duration::from_millis(300)).await;
                 exit(0);
             }
             Err(ReadlineError::Eof) => exit(0),
-            Err(e) => bail!(e),
+            Err(e) => {
+                eprintln!("{}", STRING_LOADER.str("StreamWriteError"));
+                exit(1);
+            },
         };
 
         line_reader.add_history_entry(&input).unwrap();
-        stream.write_all(input.as_bytes()).context(STRING_LOADER.str("StreamWriteError"))?;
+
+        w_server.write(input.as_bytes())
+            .await
+            .expect(&*STRING_LOADER.str("StreamWriteError"));
 
         delete_last_line();
     }
