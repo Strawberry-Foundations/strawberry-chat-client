7a7051eb5c703d3b4b5983e0211b3b4068fdadc4
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-17 17:36:01 +0100

    fixed warnings

--- src/object/login_packet.rs
+++ src/object/login_packet.rs
@@ -1,9 +1,9 @@
-use std::io::Write;
-use std::net::TcpStream;
+
+
 use serde::{Deserialize, Serialize};
-use stblib::stbm::stbchat::net::OutgoingPacketStream;
-use tokio::io::WriteHalf;
-use crate::types::{LOGIN_EVENT, STBCHAT_EVENT};
+
+
+
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EventPacket {
