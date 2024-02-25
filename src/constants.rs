5a277f0253280e3c650c1dbaa01df9e50647b347
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-25 14:02:29 +0100

    Updated version

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,3 @@
-pub const VERSION: &str = "1.3.2";
+pub const VERSION: &str = "1.3.3";
 pub const STRAWBERRY_ID_API: &str = "https://id.strawberryfoundations.xyz/v2/";
 pub const SCLOUD_API_URL: &str = "http://localhost:8000/";
