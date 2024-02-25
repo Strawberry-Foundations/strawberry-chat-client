88ad6d3af8d407420d1009d18d594dedebe088cf
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-25 13:44:08 +0100

    Added sync feature

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -1,29 +1,41 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
+use crate::auth::IdCredentials;
+use crate::constants::SCLOUD_API_URL;
+use crate::utilities::make_absolute_path;
 
 pub async fn sync() -> eyre::Result<()> {
+    let credentials = IdCredentials::new().unwrap();
+
+    let (username, auth_token) = (credentials.username, credentials.token);
+
     let client = reqwest::Client::new();
 
-    let parser: Vec<String> = std::env::args().skip(2).collect();
-    let file = parser.clone().first().unwrap().to_string();
+    let exe_path = std::env::current_exe().expect("Could not get your Strawberry Chat Client Executable");
 
-    let file_path = make_absolute_path(file.as_str());
+    let exe_dir = exe_path.parent().expect("Error determining the directory of the executable file.");
+
+    let exe_dir_str = PathBuf::from(exe_dir).display().to_string();
+
+    let mut config_path = format!("{exe_dir_str}/config.yml");
+
+    if !Path::new(&config_path).exists() {
+        config_path = String::from("./config.yml")
+    }
+
+    let file_path = make_absolute_path(config_path.as_str());
     let path = Path::new(&file_path);
 
-    let filename = if let Some(file_name) = path.file_name() {
-        if let Some(file_name_str) = file_name.to_str() {
-            file_name_str
-        } else {
-            eprintln!("Invalid filename");
-            std::process::exit(1)
-        }
-    } else {
+    /* let filename = path.file_name().map_or_else(|| {
         eprintln!("No filename found");
         std::process::exit(1)
-    };
+    }, |file_name| file_name.to_str().map_or_else(|| {
+            eprintln!("Invalid filename");
+            std::process::exit(1)
+        }, |file_name_str| file_name_str)); */
 
     println!("{}", path.to_str().unwrap());
 
-    let url = format!("{API_URL}upload/{username}@{auth_token}?filename={filename}");
+    let url = format!("{SCLOUD_API_URL}upload/{username}@{auth_token}?filename=config_stbchat.yml");
 
     // Lese den Inhalt der Datei
     let file_content = std::fs::read(file_path).unwrap();
