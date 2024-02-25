49a65c696997cafc0df6eabba1b27be1538b154b
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-25 14:01:50 +0100

    Added cloud config fetching

--- src/global.rs
+++ src/global.rs
@@ -4,9 +4,11 @@ use std::path::{Path, PathBuf};
 use lazy_static::lazy_static;
 
 use stblib::strings::Strings;
+use crate::auth::IdCredentials;
 
 use crate::config::{Config, get_lang_cfg, ServerValues};
 use crate::cli::user_server_list::user_server_list;
+use crate::constants::SCLOUD_API_URL;
 
 lazy_static! {
     pub static ref CONFIG: Config = {
@@ -22,15 +24,24 @@ lazy_static! {
             config_path = String::from("./config.yml")
         }
 
-        Config::new(config_path)
+        Config::new(config_path).unwrap_or_else(|_| {
+            let credentials = IdCredentials::new().unwrap();
+            let (username, auth_token) = (credentials.username, credentials.token);
+
+            let url = format!("{SCLOUD_API_URL}fetch/{username}@{auth_token}/config_stbchat.yml");
+
+            let content = futures::executor::block_on( async { reqwest::get(url).await.unwrap().text().await.unwrap() });
+
+            Config::new_from_content(content)
+        })
     };
 
     pub static ref STRING_LOADER: Strings = Strings::new(CONFIG.language.as_str(), &get_lang_cfg());
 
     pub static ref SERVER_CONFIG: ServerValues = {
         match CONFIG.autoserver.enabled {
-            true => return Config::server_id(CONFIG.autoserver.server_id, &CONFIG.path),
-            false => user_server_list(&CONFIG.path)
+            true => return Config::server_id(CONFIG.autoserver.server_id, &CONFIG.content),
+            false => user_server_list(&CONFIG.content)
         }
     };
 }
