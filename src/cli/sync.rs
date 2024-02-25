39b877a12810debd5d6c7e3b403181b32c46fa1b
Juliandev02 <juli.mackenstein@gmail.com>
2024-02-25 14:01:50 +0100

    Added cloud config fetching

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -9,7 +9,7 @@ pub async fn sync() -> eyre::Result<()> {
     let (username, auth_token) = (credentials.username, credentials.token);
 
     let client = reqwest::Client::new();
-
+    
     let exe_path = std::env::current_exe().expect("Could not get your Strawberry Chat Client Executable");
 
     let exe_dir = exe_path.parent().expect("Error determining the directory of the executable file.");
