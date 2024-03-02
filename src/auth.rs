008948293058215a5a312b0c4915b225eb63292e
Juliandev02 <juli.mackenstein@gmail.com>
2024-03-02 17:50:26 +0100

    General fixes

--- src/auth.rs
+++ src/auth.rs
@@ -9,22 +9,20 @@ pub struct IdCredentials {
 }
 
 impl IdCredentials {
-    pub fn new() -> Result<IdCredentials, Box<dyn std::error::Error>> {
-        if let Some(home_dir) = dirs::home_dir() {
+    pub fn new() -> Self {
+        dirs::home_dir().map_or_else(|| { std::process::exit(1); }, |home_dir| {
             let config_dir = home_dir.join(".config").join("strawberry-id");
             let credentials_path = config_dir.join("credentials.yml");
 
             if credentials_path.exists() {
-                let credentials_str = std::fs::read_to_string(&credentials_path)?;
+                let credentials_str = std::fs::read_to_string(&credentials_path).unwrap();
+                let credentials: Self = serde_yaml::from_str(&credentials_str).unwrap();;
 
-                let credentials: IdCredentials = serde_yaml::from_str(&credentials_str)?;
-
-                Ok(credentials)
+                credentials
             } else {
-                Err(format!("{RED}{BOLD}{}{RESET} {}{C_RESET}", STRING_LOADER.load("ErrorReadingCredentials"), STRING_LOADER.load("CredentialsFileNotExist")).into())
+                println!("{RED}{BOLD}{}{RESET} {}{C_RESET}", STRING_LOADER.load("ErrorReadingCredentials"), STRING_LOADER.load("CredentialsFileNotExist"));
+                std::process::exit(1);
             }
-        } else {
-            Err(format!("{RED}{BOLD}{}{RESET} {}{C_RESET}", STRING_LOADER.load("ErrorReadingCredentials"), STRING_LOADER.load("HomeDirectoryNotFound")).into())
-        }
+        })
     }
 }
