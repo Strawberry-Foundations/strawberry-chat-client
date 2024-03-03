7a94dd8f1db8184deff316c4f84c000d434292df
Juliandev02 <juli.mackenstein@gmail.com>
2024-03-03 12:53:17 +0100

    code cleaning

--- src/communication/legacy_recv.rs
+++ src/communication/legacy_recv.rs
@@ -23,3 +23,85 @@ pub fn legacy_recv(mut stream: TcpStream) -> eyre::Result<()> {
 
     Ok(())
 }
+
+/*
+    let json_iter = Deserializer::from_reader(iter_stream).into_iter::<Value>();
+
+    for json in json_iter {
+        let msg = match json {
+            Ok(j) => j,
+            Err(e) => {
+                eprintln!("Failed to deserialize json: {e}");
+                continue
+            },
+        };
+
+        match msg["message_type"].as_str() {
+            Some("system_message") => {
+                let fmt = match CONFIG.message_format.as_str() {
+                    "default" => MessageFormatter::default_system(
+                        msg["message"]["content"].as_str().unwrap(),
+                    ),
+                    _ => MessageFormatter::default_system(
+                        msg["message"]["content"].as_str().unwrap(),
+                    ),
+                };
+
+                println!("{}", fmt);
+            }
+            Some("user_message") => {
+                let fmt = match CONFIG.message_format.as_str() {
+                    "default" => MessageFormatter::default_user(
+                        msg["username"].as_str().unwrap(),
+                        msg["nickname"].as_str().unwrap(),
+                        msg["role_color"].as_str().unwrap(),
+                        crate::cli::formatter::badge_handler(msg["badge"].as_str().unwrap()).as_str(),
+                        msg["message"]["content"].as_str().unwrap(),
+                    ),
+                    _ => MessageFormatter::default_user(
+                        msg["username"].as_str().unwrap(),
+                        msg["nickname"].as_str().unwrap(),
+                        msg["role_color"].as_str().unwrap(),
+                        msg["badge"].as_str().unwrap(),
+                        msg["message"]["content"].as_str().unwrap(),
+                    ),
+                };
+
+                println!("{}", fmt);
+            }
+
+            Some("stbchat_backend") => {
+                client_meta.username = msg["user_meta"]["username"].as_str().unwrap().trim().to_string();
+            }
+
+            Some("stbchat_event") => {
+                match msg["event_type"].as_str() {
+                    Some("event.login") => {
+                        let (username, password) = if SERVER_CONFIG.autologin {
+                            (SERVER_CONFIG.credentials.username.clone(), SERVER_CONFIG.credentials.password.clone())
+                        } else {
+                            login()
+                        };
+
+                        let mut login_packet = ServerLoginCredentialsPacketClient::new(username, password);
+                        login_packet.write(stream);
+
+                        tx.send(()).unwrap();
+                    },
+                    _ => unreachable!()
+                }
+            }
+
+            None => unreachable!(),
+            m => println!(
+                "{} {YELLOW}{BOLD}{} ({})",
+                "[UImp] ".red().bold(),
+                STRING_LOADER.str("UnimplementedPacket"),
+                m.unwrap(),
+            ),
+        }
+    }
+    println!("{}", STRING_LOADER.str("CloseApplication").yellow().bold());
+    println!("{}", STRING_LOADER.str("PressCtrlDToExit").bold());
+
+    Ok(()) */
