008c3827421ab94cc764e90bab35474b8d4349b7
Julian <81520713+Juliandev02@users.noreply.github.com>
2024-03-03 17:04:28 +0100

    Fixed authentication without config file

--- src/global.rs
+++ src/global.rs
@@ -12,6 +12,40 @@ use crate::constants::SCLOUD_API_URL;
 
 lazy_static! {
     pub static ref CONFIG: Config = {
+        let headless_config = r#"language: en_US
+update_channel: "stable"
+detect_same_system_messages: true
+message_format: default
+enable_notifications: true
+enable_terminal_bell: true
+experimental_debug_mode: false
+extreme_debug_mode: false
+recv_allowed_bytes: 8192
+config_ver: 6
+
+notification:
+  use_legacy_notifier: false
+  icon_path: ""
+
+
+networking:
+  online_mode: true
+  keep_alive: true
+  latency_mode: true
+  latency_mode_time: 1
+
+autoserver:
+  enabled: false
+  server_id: 0
+
+server:
+  0:
+    name: strawberryfoundations.xyz
+    address: 45.131.109.170
+    port: 49200
+    type: Main"#;
+
+
         let exe_path = env::current_exe().expect("Could not get your Strawberry Chat Client Executable");
 
         let exe_dir = exe_path.parent().expect("Error determining the directory of the executable file.");
@@ -25,18 +59,21 @@ lazy_static! {
         }
 
         Config::new(config_path).unwrap_or_else(|_| {
-            let credentials = IdCredentials::new();
+            let credentials = match IdCredentials::new() {
+                Ok(credentials) => credentials,
+                Err(_) => return Config::new_from_content(String::from(headless_config))
+            };
+
             let (username, auth_token) = (credentials.username, credentials.token);
 
             let url = format!("{SCLOUD_API_URL}fetch/{username}@{auth_token}/config_stbchat.yml");
-
             let content = futures::executor::block_on( async { reqwest::get(url).await.unwrap().text().await.unwrap() });
 
             Config::new_from_content(content)
         })
     };
 
-    pub static ref STRING_LOADER: Strings = Strings::new(CONFIG.language.as_str(), &get_lang_cfg());
+    pub static ref STRING_LOADER: Strings = Strings::new(Config::load_language().as_str(), &get_lang_cfg());
 
     pub static ref SERVER_CONFIG: ServerValues = {
         match CONFIG.autoserver.enabled {
