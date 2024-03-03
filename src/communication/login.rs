3e3dd2f83659278515fe7b7c25d9e78a13c14142
Juliandev02 <juli.mackenstein@gmail.com>
2024-03-03 13:20:30 +0100

    added register

--- src/communication/login.rs
+++ src/communication/login.rs
@@ -2,10 +2,15 @@ use std::process::exit;
 use std::thread::sleep;
 use std::time::Duration;
 use rustyline::error::ReadlineError;
+use crate::communication::register::register;
 use crate::global::STRING_LOADER;
 
+pub enum PreAuthEvent {
+    Login(String, String),
+    Register(String, String, String)
+}
 
-pub fn login() -> (String, String) {
+pub fn login() -> PreAuthEvent {
     let mut line_reader = rustyline::DefaultEditor::new().unwrap();
 
     /* let stdin = std::io::stdin();
@@ -35,6 +40,11 @@ pub fn login() -> (String, String) {
         Err(ReadlineError::Eof) => exit(0),
         Err(_) => exit(1),
     };
+    
+    if username == "register" {
+        let (username, password, role_color) = register();
+        return PreAuthEvent::Register(username, password, role_color)
+    }
 
     let password: String = match line_reader.readline(STRING_LOADER.load("Password").as_str()) {
         Ok(i) => i,
@@ -46,5 +56,5 @@ pub fn login() -> (String, String) {
         Err(_) => exit(1),
     };
 
-    (username, password)
+    PreAuthEvent::Login(username, password)
 }
