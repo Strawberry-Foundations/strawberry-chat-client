980307fe0e20570964dee7b3d50d4596e355afdd
Juliandev02 <julian@strawberryfoundations.xyz>
2024-08-10 12:30:40 +0200

    fix: add default for ClientMeta

--- src/object/client_meta.rs
+++ src/object/client_meta.rs
@@ -2,6 +2,12 @@ pub struct ClientMeta {
     pub username: String,
 }
 
+impl Default for ClientMeta {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl ClientMeta {
     pub fn new() -> Self {
         Self {
