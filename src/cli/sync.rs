b00315d463e835e9d6f1c848d7083ba3ea0a6e34
Juliandev02 <julian@strawberryfoundations.org>
2024-09-13 17:16:31 +0200

    feat: some small optimizations

--- src/cli/sync.rs
+++ src/cli/sync.rs
@@ -30,29 +30,18 @@ pub async fn sync() -> eyre::Result<()> {
     let file_path = make_absolute_path(config_path.as_str());
     let path = Path::new(&file_path);
 
-    /* let filename = path.file_name().map_or_else(|| {
-        eprintln!("No filename found");
-        std::process::exit(1)
-    }, |file_name| file_name.to_str().map_or_else(|| {
-            eprintln!("Invalid filename");
-            std::process::exit(1)
-        }, |file_name_str| file_name_str)); */
-
     println!("{}", path.to_str().unwrap());
 
     let url = format!("{STRAWBERRY_CLOUD_API_URL}upload/{username}@{auth_token}?filename=config_stbchat.yml");
 
-    // Lese den Inhalt der Datei
     let file_content = std::fs::read(file_path).unwrap();
 
-    // Sende den Dateiinhalt an den Server
     let response = client.post(url)
         .header("Content-Type", "multipart/form-data")
         .body(file_content)
         .send()
         .await?;
 
-    // Gib die Serverantwort aus
     println!("{}", response.text().await?);
 
     Ok(())
